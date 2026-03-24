94b0b86679ee6af9d59173d52dd4749616f2361e
mintacgabriel-lgtm <mintacgabriel@gmail.com>
2026-03-24 20:01:27 +0800

    Gab Phone

--- Phone.java
+++ Phone.java
@@ -1,1 +1,13 @@
+package org.example;
 
+public class Phone extends Hardware {
+
+    public Phone(int id, String brand, int spec) {
+        super(id, brand, spec);
+    }
+
+    @Override
+    public String interpretSpec() {
+        return getSpec() + " Megapixels";  // use getter, not spec directly
+    }
+}
