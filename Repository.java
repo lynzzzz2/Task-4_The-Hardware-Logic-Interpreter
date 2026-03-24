6b206bbad17ec1f83578d1e6dfd1dde59f04b520
mintacgabriel-lgtm <mintacgabriel@gmail.com>
2026-03-24 19:57:43 +0800

    Gab Repo

--- Repository.java
+++ Repository.java
@@ -56,17 +56,3 @@ public class Repository {
         return list;
     }
 }
-
-package org.example;
-
-public class Phone extends Hardware {
-
-    public Phone(int id, String brand, int spec) {
-        super(id, brand, spec);
-    }
-
-    @Override
-    public String interpretSpec() {
-        return getSpec() + " Megapixels";  // use getter, not spec directly
-    }
-}
